497acbe4d0d7958b534688d523bfe661cc12877c
Anthony.battista <Anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-15 15:04:20 -0700

    Home Commit

---
+++ Final Project/src/finalProject.java
@@ -0,0 +1,49 @@
+
+public class finalProject
+	{
+		private String suit;
+		private String type;
+		private int value;
+		
+		public finalProject(String s, String t, int v)
+		{
+			suit = s;
+			type = t;
+			value = v;
+			
+		}
+
+		public String getSuit()
+			{
+				return suit;
+			}
+
+		public void setSuit(String suit)
+			{
+				this.suit = suit;
+			}
+
+		public String getType()
+			{
+				return type;
+			}
+
+		public void setType(String type)
+			{
+				this.type = type;
+			}
+
+		public int getValue()
+			{
+				return value;
+			}
+
+		public void setValue(int value)
+			{
+				this.value = value;
+			}
+			
+		
+
+		
+	}
